50fe9a3318c14ff33dcba426d82cbed2fd7b2a60
Dvir Avrahami <dviravr@gmail.com>
2022-03-15 15:08:36 +0200

    to bits project

---
+++ toBits/cmake-build-debug/CMakeFiles/toBits.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/toBits.dir/main.cpp.o"
+  "toBits"
+  "toBits.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/toBits.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ toBits/main.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+string printbincharpad(char c) {
+    string byte;
+    for (int i = 7; i >= 0; --i) {
+        byte += (c & (1 << i)) ? '1' : '0';
+    }
+    return byte;
+}
+
+void readFile(string& srcFile, string &destFile) {
+    char c;
+    ifstream input_file(srcFile);
+    ofstream output_file(destFile);
+    if (!input_file.is_open()) {
+        cerr << "Could not open the file - '" << srcFile << "'" << endl;
+        return;
+    }
+    if (!output_file.is_open()) {
+        cerr << "Could not open the file - '" << destFile << "'" << endl;
+        return;
+    }
+    while (input_file.get(c)) {
+        output_file << printbincharpad(c);
+    }
+    cout << endl;
+    input_file.close();
+    output_file.close();
+}
+
+
+int main() {
+    string filesArray[6] = {
+//            "../text/sam/sam.txt",
+            "../text/pitches/pitches.50MB",
+            "../text/dna/dna.50MB",
+            "../text/english/english.50MB",
+            "../text/proteins/proteins.50MB",
+            "../text/sources/sources.50MB",
+            "../text/xml/xml.50MB"
+    };
+    string decodedFilesArray[6] = {
+//            "../text/sam/bits_output.txt",
+            "../text/pitches/bits_output.txt",
+            "../text/dna/bits_output.txt",
+            "../text/english/bits_output.txt",
+            "../text/proteins/bits_output.txt",
+            "../text/sources/bits_output.txt",
+            "../text/xml/bits_output.txt"
+    };
+
+    for (int i = 0; i < filesArray->length(); ++i) {
+        readFile(filesArray[i], decodedFilesArray[i]);
+    }
+    return 0;
+}
